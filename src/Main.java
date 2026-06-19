e733750383ca65ffc5066b83579457f97f41cd3e
gayo73 <gayoung2037@gmail.com>
2026-06-19 15:50:31 +0900

    feat: Main 추가

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import controller.AccountController;
+import repository.AccountRepository;
+import service.AccountService;
+import view.AccountView;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		AccountRepository repository = new AccountRepository();
+		AccountService service = new AccountService(repository);
+		AccountView view = new AccountView();
+		AccountController controller = new AccountController(service, view);
+		
+		controller.run();
 	}
 }

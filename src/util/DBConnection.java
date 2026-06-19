1510b0e08499a14b7b06ddc6d58508fc3609cf31
YANG YEEUN <beautifulsilversheep@gmail.com>
2026-06-19 16:09:19 +0900

    refactor: DBConnection을 util 패키지로 이동

--- src/util/DBConnection.java
+++ src/util/DBConnection.java
@@ -1,8 +1,8 @@
+package util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import javax.management.RuntimeErrorException;
 
 public class DBConnection {
 	private static final String url = "jdbc:oracle:thin:@localhost:1521:xe";
